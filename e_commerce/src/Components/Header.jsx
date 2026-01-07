d6ebccd286af537c9dc25af901d7a4a6536e6989
shawon-1998 <billalhossainshawon989@gmail.com>
2026-01-07 18:51:25 +0600

    dropdown

--- e_commerce/src/Components/Header.jsx
+++ e_commerce/src/Components/Header.jsx
@@ -11,9 +11,12 @@ const Header = () => {
                         <div className='w-[77%] lg:flex font-pop justify-end font-normal text-sm gap-2 '>
                             <p className='lg:flex gap-2 '>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! <a href="#" className='font-semibold '>ShopNow</a></p>        
                         </div>
-                        <Flex className='w-[23%] justify-end flex-none gap-1'>
-                            <p>English</p>
-                            <p className='text-lg'><MdKeyboardArrowDown /></p>
+                        <Flex className='w-[23%] justify-end flex-none gap-1 '>
+                            <select name="cars" id="cars" className='bg-black text-white outline-none'>
+                            <option value="volvo">English</option>
+                            <option value="saab">Bangla</option>
+                        </select>
+
                         </Flex>
                     </Flex>
                 </div>
