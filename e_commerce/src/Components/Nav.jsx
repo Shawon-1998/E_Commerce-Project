31481747f1d3cd72c3cf2eab92bc0e6fdff6b72a
shawon-1998 <billalhossainshawon989@gmail.com>
2026-02-08 23:57:18 +0600

    nav responsive

--- e_commerce/src/Components/Nav.jsx
+++ e_commerce/src/Components/Nav.jsx
@@ -67,27 +67,26 @@ const Nav = ({ className }) => {
         </div>
       </nav>
 
-      <nav className=' block pt-11.75 pb-5.75 border-[#0000003d] border-b font-pop'>
+      <nav className=' pt-11.75 pb-5.75 border-[#0000003d] border-b font-pop '>
         <Container>
-
-          <div className='flex md:items-center justify-between'>
-            <div className='w-[40%] md:w-[20%]'>
+          <div className='flex md:items-center   justify-between '>
+            <div className=''>
               <NavLink to="/" end> <img src={logo} alt="" /></NavLink>
             </div>
-            <ul className='md:flex gap-6 lg:gap-12 font-normal'>
-              <NavLink to="/" end>Home</NavLink>
+             <div className='md:flex gap-5 relative md:items-center'>
+            <ul className='md:flex bg-black text-white md:bg-transparent md:text-black gap-6 lg:gap-12 font-normal md:static absolute 
+             top-5 -right-30 py-3 px-3 '>
+              <li><NavLink to="/" end>Home</NavLink></li>
               <li><NavLink to="Contact" end> Contact </NavLink></li>
               <li><NavLink to="/" end>About</NavLink></li>
               <li>Sign Up</li>
             </ul>
-
-          </div>
-          <Flex>
+          <div className='md:flex hidden'>
             <div className='relative' >
               <input type="text" placeholder='What are you looking for?' className=' w-50 md:w-55 lg:w-60.75 py-2.5 md:ps-4 ps-2 bg-[#F5F5F5] rounded[4px] placeholder:text-xs outline-none relative' />
               <HiMiniMagnifyingGlass className='absolute top-0 right-0 text-lg' />
             </div>
-            <Flex className='flex gap-4 text-xl'>
+            <Flex className='flex gap-4 text-xl '>
               <div className='relative'>
                 <IoHeartOutline className='text-[24px] opacity-80 cursor-pointer' onClick={handleWish} />
                 <div className='absolute -top-1 -right-2 h-4 w-4 font-extralight bg-primary rounded-full flex items-center justify-center text-[10px] text-white'>
@@ -101,9 +100,10 @@ const Nav = ({ className }) => {
                 </div>
               </div>
             </Flex>
-          </Flex>
-
-
+          </div>
+             </div>
+           <FaBarsStaggered className='md:hidden' />
+          </div>
         </Container>
       </nav>
     </>
