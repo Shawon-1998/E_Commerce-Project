c84b5f0b2901b837d113f6cc449aed841736d142
shawon-1998 <billalhossainshawon989@gmail.com>
2026-02-09 01:40:43 +0600

    override

--- e_commerce/src/Components/Nav.jsx
+++ e_commerce/src/Components/Nav.jsx
@@ -81,17 +81,17 @@ const Nav = ({ className }) => {
             <div className=''>
               <NavLink to="/" end> <img src={logo} alt="" /></NavLink>
             </div>
-            <div className='md:flex gap-5 relative md:items-center'>
-              <ul className={` ${change ? "block" : "hidden"} md:flex bg-black text-white md:bg-transparent md:text-black gap-6  lg:gap-12 font-normal md:static absolute top-5 -right-36 py-3 px-3 hover:transition-all rounded-sm leading-10 `}>
+            <div className='md:flex gap-5 relative md:items-center justify-between'>
+              <ul className={` ${change ? "block" : "hidden"} md:flex bg-black text-white md:bg-transparent md:text-black gap-6  lg:gap-12 font-normal md:static absolute top-5 left-20 py-3 px-3 hover:transition-all rounded-sm leading-10 `}>
                 <li><NavLink to="/" end>Home</NavLink></li>
                 <li><NavLink to="Contact" end> Contact </NavLink></li>
                 <li><NavLink to="/" end>About</NavLink></li>
                 <li>Sign Up</li>
               </ul> 
               <div className={`md:flex ${shopping ? "block" : "hidden" } `}>
-                <div className='relative md:ms-0 md:me-2 lg:ms-37 lg:me-6' >
-                  <input type="text" placeholder='What are you looking for?' className=' w-50 md:w-45 lg:w-60.75 py-2.5 md:ps-4 ps-2 bg-[#F5F5F5] rounded[4px] md:placeholder:text-[10px] placeholder:text-xs outline-none relative' />
-                  <HiMiniMagnifyingGlass className='absolute top-4 right-4 text-lg' />
+                <div className='relative md:ms-0 md:me-2 lg:ms-34 lg:me-6' >
+                  <input type="text" placeholder='What are you looking for?' className=' w-50 md:w-45 lg:w-60.75 md:py-2.5 md:ps-4 ps-2 bg-[#F5F5F5] rounded[4px] md:placeholder:text-[10px] placeholder:text-xs outline-none relative' />
+                  <HiMiniMagnifyingGlass className='absolute top-1 md:top-4 right-4 text-lg' />
                 </div>
                 <Flex className='flex gap-4 text-xl mt-3 '>
                   <div className='relative'>
@@ -102,17 +102,17 @@ const Nav = ({ className }) => {
                   </div>
                   <div className='relative'>
                     <IoCartOutline className='text-[24px] opacity-80 cursor-pointer' onClick={handleCart} />
-                    <div className='absolute -top-1 -right-2 h-4 w-4 font-extralight bg-primary rounded-full flex items-center justify-center text-[10px] text-white'>
+                    <div className='absolute -top-1 -right-2 h-4 w-4 font-medium bg-primary rounded-full flex items-center justify-center text-[10px] text-white'>
                       {Data.length}
                     </div>
                   </div>
                 </Flex>
               </div>
             </div>
-          <div>
+       
+            <FaShoppingBasket className='md:hidden' onClick={handleShopping} />
               <FaBarsStaggered className='md:hidden' onClick={handleToggle} />
-            <FaShoppingBasket className='md:hidden mt-5' onClick={handleShopping} />
-          </div>
+       
           </div>
         </Container>
       </nav>
