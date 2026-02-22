fe6c4e9ff2939bc45515c1512dc527703c31422e
shawon-1998 <billalhossainshawon989@gmail.com>
2026-02-22 23:42:51 +0600

    added toastify

--- e_commerce/src/Components/CardSec.jsx
+++ e_commerce/src/Components/CardSec.jsx
@@ -9,22 +9,36 @@ import { cartData, removeWishReducer, wishList } from '../slices/ProductSlice';
 import { Bounce, toast } from 'react-toastify';
 
 
-const CardSec = ({ productName, ImgSrc, price, rating, discountPrice, digit, discount, className, id, cart,remove,className1 }) => {
-    
+const CardSec = ({ productName, ImgSrc, price, rating, discountPrice, digit, discount, className, id, cart, remove, className1 }) => {
+
     const wishProducts = useSelector((state) => state.Products.wish)
     const cartProducts = useSelector((state) => state.Products.cart)
 
-     const notify = () =>toast.success('Successfully added', {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: false,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-        transition: Bounce,
-        });
+    const notify = (matchItem) => {
+        matchItem == undefined ?
+            toast.success('Successfully added', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                transition: Bounce,
+            }) :
+            toast.warn('Already Added!', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+                transition: Bounce,
+            })
+    }
 
     const dispatch = useDispatch()
     let navigate = useNavigate();
@@ -32,24 +46,33 @@ const CardSec = ({ productName, ImgSrc, price, rating, discountPrice, digit, dis
         navigate(`/Details/${id}`)
     }
     const handleCart = () => {
-         cartProducts.find((findItem) => (findItem.id== id)) ? 
-         dispatch(!cartData(cart)) : dispatch(cartData(cart)) 
-        notify()
+        //  cartProducts.find((findItem) => (findItem.id== id)) ? 
+        //  dispatch(!cartData(cart)) : dispatch(cartData(cart)) 
+        // notify()
+        const matchItem = wishProducts.find((findItem) => (findItem.id == id))
+        !matchItem ? dispatch(wishList(cart)) : null
+        notify(matchItem)
     }
     const handleWish = () => {
-       wishProducts.find((findItem) => (findItem.id== id)) ? 
-       dispatch(!wishList(cart)) : dispatch(wishList(cart))
-       notify()
+        //    wishProducts.find((findItem) => (findItem.id== id)) ? 
+        //    dispatch(!wishList(cart)) : dispatch(wishList(cart))
+
+        const matchItem = wishProducts.find((findItem) => (findItem.id == id))
+        !matchItem ? dispatch(wishList(cart)) : null
+        notify(matchItem)
     }
-     const handleRemove=()=>{
-         dispatch(removeWishReducer(id))
-     }
+    const handleRemove = () => {
+        dispatch(removeWishReducer(id))
+        
+      !matchItem ? dispatch(wishList(cart)) : null
+       notify(matchItem)
+    }
 
     return (
         <>
-         <div>
-            
-         </div>
+            <div>
+
+            </div>
             <div className='w-67.5 mx-auto group relative'>
                 <div className='relative overflow-hidden'>
                     <img src={ImgSrc} alt="" className='w-full cursor-pointer' onClick={handleProductDetails} />
@@ -64,7 +87,7 @@ const CardSec = ({ productName, ImgSrc, price, rating, discountPrice, digit, dis
                     </div>
                     <button onClick={handleCart} className='absolute bg-black text-white block w-full py-2 px-21.75 font-pop cursor-pointer -bottom-10 group-hover:bottom-0 ease-linear duration-400'> Add to cart</button>
                 </div>
-                 <span className={`${className} w-4 h-4 text-xs  text-white absolute top-0 -left-5 rounded-full items-center flex justify-center cursor-pointer`} onClick={handleRemove}>{remove}</span>
+                <span className={`${className} w-4 h-4 text-xs  text-white absolute top-0 -left-5 rounded-full items-center flex justify-center cursor-pointer`} onClick={handleRemove}>{remove}</span>
                 <h3 className='font-medium mt-4 '>{productName}</h3>
                 <Flex className='my-2 gap-3'>
                     <p className='text-primary '>${discountPrice}</p>
