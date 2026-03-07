cc73aafbc280bb8c0c59a1ab4003812bdb8cf9f7
shawon-1998 <billalhossainshawon989@gmail.com>
2026-03-07 22:41:51 +0600

    added flex

--- e_commerce/src/Components/NewArrival.jsx
+++ e_commerce/src/Components/NewArrival.jsx
@@ -11,43 +11,43 @@ const NewArrival = () => {
           className=' text-center block mx-auto mb-5 lg:mb-15'
           className2='justify-center '
         />
-        <div className=' grid-main text-white '>
+        <div className=' grid-main md:grid md:grid-cols-[50%_auto_auto_auto] md:gap-5  text-white flex flex-col gap-5 '>
           <div className=' w-full bg-[url(assets/Frame1.png)] bg-no-repeat bg-center bg-cover rounded-sm'>
-             <NewArrivalCard
-                className='lg:pt-114.5 pt-40 ps-8 pb-9'
-                className2='py-4 lg:w-60.5 text-sm font-pop leading-5.25 font-normal'
-                productName='PlayStation 5'
-                description='Black and White version of the PS5 coming out on sale.'
-              />
+            <NewArrivalCard
+              className='lg:pt-114.5 pt-40 ps-8 pb-9'
+              className2='py-4 lg:w-60.5 text-sm font-pop leading-5.25 font-normal'
+              productName='PlayStation 5'
+              description='Black and White version of the PS5 coming out on sale.'
+            />
           </div>
-        
-            <div className=' bg-[url(assets/Frame2.png)] bg-no-repeat bg-center bg-cover w-full rounded-sm'>
-              <NewArrivalCard
-                className='lg:pt-34.5 pt-15 pb-6.5 ps-6 '
-                className2='py-4 lg:w-60.5 text-sm font-pop leading-5.25 font-normal'
-                productName='Women’s Collections'
-                description='Featured woman collections that give you another vibe.'
-              />
-            </div>
-              <div className='  bg-[url(assets/Frame3.png)] bg-no-repeat bg-center bg-cover rounded-sm'>
-                <NewArrivalCard
-                  className='lg:pt-43.75 pt-17 pb-6.5 ps-6 '
-                  productName='Speakers'
-                  className2='py-2 lg:w-60.5'
-                  description='Amazon wireless speakers'
-                />
-              </div>
-              <div className=' rounded-sm bg-[url(assets/Frame4.png)] bg-no-repeat bg-center bg-cover '>
-                <NewArrivalCard
-                  className='lg:pt-43.75 pt-15 pb-6.5 ps-6 '
-                  productName='Perfume'
-                   className2='py-2 lg:w-60.5'
-                  description='Gucci intense oud edp'
-                />
-              </div>
+
+          <div className=' bg-[url(assets/Frame2.png)] bg-no-repeat bg-center bg-cover w-full rounded-sm'>
+            <NewArrivalCard
+              className='lg:pt-34.5 pt-15 pb-6.5 ps-6 '
+              className2='py-4 lg:w-60.5 text-sm font-pop leading-5.25 font-normal'
+              productName='Women’s Collections'
+              description='Featured woman collections that give you another vibe.'
+            />
+          </div>
+          <div className='  bg-[url(assets/Frame3.png)] bg-no-repeat bg-center bg-cover rounded-sm'>
+            <NewArrivalCard
+              className='lg:pt-43.75 pt-17 pb-6.5 ps-6 '
+              productName='Speakers'
+              className2='py-2 lg:w-60.5'
+              description='Amazon wireless speakers'
+            />
+          </div>
+          <div className=' rounded-sm bg-[url(assets/Frame4.png)] bg-no-repeat bg-center bg-cover '>
+            <NewArrivalCard
+              className='lg:pt-43.75 pt-15 pb-6.5 ps-6 '
+              productName='Perfume'
+              className2='py-2 lg:w-60.5'
+              description='Gucci intense oud edp'
+            />
           </div>
         </div>
-  
+      </div>
+
     </>
   )
 }
